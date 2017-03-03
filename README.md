37d561273972529cf7b15ca91e8abd3c8c6cf14d
Kaz Murdock <3kgven@gmail.com>
2017-03-03 01:18:57 -0500

    primer commit

--- README.md
+++ README.md
@@ -1,2 +1,77 @@
-# react-body-images
-A way to add one background images to body, or create body background slider. Based on react-body-classname. So, you can manipulate your body's className too
+React Body Images
+====================
+
+A way to add one background image to body, or create body background slider in a single-page app. Based on Iestyn William's project react-body-className. So, you can manipulate your body's className too
+
+This component can be use the className feature on server side as well.
+
+Built with [React Side Effect](https://github.com/gaearon/react-side-effect).
+
+====================
+
+## Installation
+
+```
+npm install --save react-body-images
+```
+
+Dependencies: React >= 0.13.0
+
+## Features
+
+* Does not emit DOM, not even a `<noscript>`;
+* Like a normal React compoment, can use its parent's `props` and `state`;
+* Can be defined in many places throughout the application;
+* Supports arbitrary levels of nesting, combining each className;
+* Works just as well with isomorphic apps.
+
+## Example
+
+```jsx
+class SomeComponent {
+  render() {
+    // This will add an array of background images to the body
+	// And add className 'home' too
+    return (
+      <BodyImages className='home' bgImageArray={this.state.images} transitionSeconds={15}>
+        <h1>Hogar, dulce hogar.</h1>
+      </BodyImages>
+    );
+  }
+}
+
+class App {
+  render() {
+    // This will add an array of background images to the body
+	// And add className 'app' too
+    return (
+      <BodyImages className='app' bgImageArray={this.state.images} transitionSeconds={15}>
+        <SomeComponent/>
+      </BodyImages>
+    );
+    // Becuase we nested the component, our body will now replace the background images especified in SomeComponent and have the 'app home' as the class name
+  }
+}
+```
+
+Use CSS modules with webpack or similar?
+
+```jsx
+import styles from './some.css';
+
+class Thing {
+  render() {
+    return (
+	  <BodyImages className={styles.body} bgImageArray={this.state.images} transitionSeconds={15}>
+        <h1>CSS modules rock!</h1>
+      </BodyImages>
+    );
+  }
+}
+```
+
+## Server Usage
+
+If you use it on server the className feature , call `BodyClassName.rewind()` **after rendering components to string** to retrieve the combined class name. You can then embed this className into HTML page template.
+
+Because this component keeps track of mounted instances, **you have to make sure to call `rewind` on server**, or you'll get a memory leak.
---
+++ index.js
@@ -0,0 +1,77 @@
+'use strict';
+
+var React = require('react'),
+  withSideEffect = require('react-side-effect');
+
+function reducePropsToState(propsList) {
+  var transitionSeconds = 4;
+  var arrayImages = [];
+  var result = propsList.map(function(props) {
+    arrayImages = props.bgImageArray;
+    if(typeof(props.transitionSeconds)!="undefined"){
+      transitionSeconds = props.transitionSeconds;
+    }
+    return props.className;
+  }).filter(function (value, index, self) {
+    return self.indexOf(value) === index;
+  }).join(' ');
+
+  return {className:result,transitionSeconds:transitionSeconds,bgImageArray:arrayImages};
+}
+
+function backgroundSequence(bgImageArray,secs) {
+  window.clearTimeout();
+  var k = 0;
+  for (var i = 0; i < bgImageArray.length; i++) {
+    setTimeout(function(){
+      document.body.style.background = "url(" + bgImageArray[k] + ") no-repeat center center fixed";
+      document.body.style.backgroundSize ="cover";
+      document.body.style.backgroundColor = "#999";
+      if ((k + 1) === bgImageArray.length) { setTimeout(function() { backgroundSequence(bgImageArray,secs) }, (secs * 1000))} else { k++; }
+    }, (secs * 1000) * i)
+  }
+}
+
+function handleStateChangeOnClient(propiedades) {
+  for (var key in propiedades) {
+    if(key=="className"){
+      document.body.className = propiedades[key] || '';
+    }else if(key=="bgImageArray" && typeof(propiedades[key])!="undefined" && propiedades[key].hasOwnProperty("length") && propiedades[key].length>0){
+      if(propiedades[key].length==1) {
+        console.log("cargando imagen:"+propiedades[key][0]);
+        new Image().src = propiedades[key][0];
+        document.body.style.background = "url("+propiedades[key][0]+") no-repeat center center fixed";
+        document.body.style.backgroundSize = "cover";
+        document.body.style.backgroundColor = "#999";
+      }else {
+        propiedades[key].forEach(function(img){
+          new Image().src = img;
+          // caches images, avoiding white flash between background replacements
+        });
+
+        backgroundSequence(propiedades[key],propiedades["transitionSeconds"]);
+      }
+    }
+  }
+}
+
+var DocumentTitle = React.createClass({
+  propTypes: {
+    className: React.PropTypes.string.isRequired,
+    bgImageArray: React.PropTypes.array.isRequired,
+    transitionsSeconds: React.PropTypes.number
+  },
+
+  render: function render() {
+    if (this.props.children) {
+      return React.Children.only(this.props.children);
+    } else {
+      return null;
+    }
+  }
+});
+
+module.exports = withSideEffect(
+  reducePropsToState,
+  handleStateChangeOnClient
+)(DocumentTitle);
